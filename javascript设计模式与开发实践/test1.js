077582097889e5a9f727933d9cb7d1ade4c2174e
shellteo <shellteo@163.com>
2017-10-27 17:42:03 +0800

    feat: 对象克隆

--- javascript设计模式与开发实践/test1.js
+++ javascript设计模式与开发实践/test1.js
@@ -36,3 +36,34 @@ Chicken.prototype.sound = function(){
 
 makeSound( new Duck() ); // 嘎嘎嘎
 makeSound( new Chicken() ); // 咯咯咯
+
+/**
+ * 增加类型检查，这对于处理后台的返回值来说至关重要，
+ * 因为后台可能返回不是我们想要的类型，所以要实现类型检查进行错误处理
+ *
+ * 在电影的拍摄现场，当导演喊出“action”时，主角开始背台词，照明师负责打灯
+ 光，后面的群众演员假装中枪倒地，道具师往镜头里撒上雪花。在得到同一个消息时，
+ 每个对象都知道自己应该做什么。如果不利用对象的多态性，而是用面向过程的方式来
+ 编写这一段代码，那么相当于在电影开始拍摄之后，导演每次都要走到每个人的面前，
+ 确认它们的职业分工（类型），然后告诉他们要做什么。如果映射到程序中，那么程序
+ 中将充斥着条件分支语句。
+ * */
+
+var Plane = function(){
+    this.blood = 100;
+    this.attackLevel = 1;
+    this.defenseLevel = 1;
+};
+let plane = new Plane();
+console.log(plane);
+plane.blood = 500;
+plane.attackLevel = 10;
+plane.defenseLevel = 7;
+console.log(plane);
+let clonePlane = Object.create(plane);
+Object.create = Object.create || function( obj ){
+    let F = function(){};
+    F.prototype = obj;
+    return new F();
+}
+console.log( clonePlane.attackLevel );
