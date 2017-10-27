2152599e9831c2db8285a7a4dee02c27fa73ce2d
shellteo <shellteo@163.com>
2017-10-27 18:07:26 +0800

    feat:

--- javascript设计模式与开发实践/test1.js
+++ javascript设计模式与开发实践/test1.js
@@ -49,6 +49,9 @@ makeSound( new Chicken() ); // 咯咯咯
  中将充斥着条件分支语句。
  * */
 
+/**
+ * 原型克隆
+ * */
 var Plane = function(){
     this.blood = 100;
     this.attackLevel = 1;
@@ -67,3 +70,34 @@ Object.create = Object.create || function( obj ){
     return new F();
 }
 console.log( clonePlane.attackLevel );
+
+/*
+
+function Person( name ){
+    this.name = name;
+}
+Person.prototype.getName = function(){
+    return this.name;
+}
+let a = new Person( 'sven' )
+console.log( a.name ); // 输出：sven
+console.log( a.getName() ); // 输出：sven
+console.log( Object.getPrototypeOf( a ) === Person.prototype );*/
+
+function Person( name ){
+    this.name = name;
+}
+Person.prototype.getName = function(){
+    return this.name;
+}
+let objectFactory = function(){
+    let obj = new Object(), // 从Object.prototype 上克隆一个空的对象
+        Constructor = [].shift.call( arguments ); // 取得外部传入的构造器，此例是Person
+    obj.__proto__ = Constructor.prototype; // 指向正确的原型
+    let ret = Constructor.apply( obj, arguments ); // 借用外部传入的构造器给obj 设置属性
+    return typeof ret === 'object' ? ret : obj; // 确保构造器总是会返回一个对象
+};
+let a = objectFactory( Person, 'sven' );
+console.log( a.name ); // 输出：sven
+console.log( a.getName() ); // 输出：sven
+console.log( Object.getPrototypeOf( a ) === Person.prototype ); // 输出：true
